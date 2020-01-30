1bcf5dbeccee6fead986b0e44b7ae398a18c9a27
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2020-01-30 17:26:18 -0200

    Setup AdonisJS

--- api/app/config/env.ts
+++
@@ -1,9 +0,0 @@
-import env from 'dotenv';
-
-env.config();
-
-export class Env {
-  public static get(e: any): any {
-    return process.env[e];
-  }
-}
---
+++ api/database/seeds/DatabaseSeeder.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| DatabaseSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class DatabaseSeeder {
+  async run () {
+    await Factory
+      .model('App/Models/User')
+      .createMany(10)
+  }
+}
+
+module.exports = DatabaseSeeder
