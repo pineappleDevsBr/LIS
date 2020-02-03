e8cfefa2b2ea40617d4a3a343d97fce0851a90d2
fernandosouzamarques <fernando.souza@novahaus.com.br>
2020-02-03 10:59:07 -0300

    Modal for buy product

---
+++ site/src/components/buy-product.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-dialog v-model="buy">
+    <q-card class="m-card m-buy">
+      <q-card-section class="m-buy_cash">
+        Preço: {{product.price}}
+        <img class="m-buy_cash-icon" :src="`statics/store/coin.svg`" alt="">
+      </q-card-section>
+      <q-card-section class="m-buy_product">
+        <h2 class="m-buy_product-name">{{product.name}}</h2>
+        <img class="m-buy_product_icon" :src="`statics/store/products/${product.icon}`" alt="">
+        <p class="m-buy_product_description">{{product.description}}</p>
+      </q-card-section>
+
+      <q-card-actions class="m-buy_add">
+        Quantidade: {{qtde}}
+        <q-btn class="m-buy_add-btn" push round dense icon="add" @click="qtde += 1" />
+      </q-card-actions>
+
+      <q-card-section class="m-buy_value">
+        Valor total: {{value}}
+      </q-card-section>
+      <q-separator />
+
+      <q-card-actions align="right">
+        <q-btn v-close-popup flat class="m-buy_btn" label="Cancelar" />
+        <q-btn v-close-popup flat class="m-buy_btn" label="Comprar" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'buy',
+  props: {
+    buy: Boolean,
+    product: Object
+  },
+  data () {
+    return {
+      qtde: 1
+    }
+  },
+  computed: {
+    value () {
+      return this.qtde * this.product.price
+    }
+  }
+}
+</script>
--- site/src/pages/store.vue
+++ site/src/pages/store.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="m-cards">
-    <q-card class="m-card" v-for="item in products" v-bind:key="item.id">
+    <q-card class="m-card" v-for="item in products" v-bind:key="item.id" @click="buyProduct(item.id)">
       <q-card-section class="m-store_box-product">
         <div class="m-store_price">
           <strong>{{item.price}}</strong>
-          <img class="m-store_coin" :src="`statics/store/coin.svg`" alt="">
+          <img class="m-store_coin" :src="`statics/store/coin.svg`" alt="products[1]">
         </div>
         <img class="m-store_icon" :src="`statics/store/products/${item.icon}`" alt="">
         <div class="m-store_product">
@@ -19,21 +19,35 @@
         </div>
       </q-card-section>
     </q-card>
+    <buy :buy="buyConfirm" :product="products[productId]"></buy>
   </div>
 </template>
 
 <script>
+import buy from '../components/buy-product'
+
 export default {
   name: 'Store',
+  components: {
+    buy
+  },
   data () {
     return {
+      buyConfirm: false,
+      productId: 0,
       products: [
-        { id: 1, name: 'Vale-presente', price: 100, icon: 'buy.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
-        { id: 2, name: 'Vale-presente', price: 100, icon: 'buy.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
-        { id: 3, name: 'Vale-presente', price: 100, icon: 'buy.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
-        { id: 4, name: 'Vale-presente', price: 100, icon: 'buy.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
-        { id: 5, name: 'Vale-presente', price: 100, icon: 'buy.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' }
+        { id: 1, name: 'Vale-presente', price: 100, icon: 'gift-card.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
+        { id: 2, name: 'Vale-presente', price: 100, icon: 'gift-card.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
+        { id: 3, name: 'Vale-presente', price: 100, icon: 'gift-card.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
+        { id: 4, name: 'Vale-presente', price: 100, icon: 'gift-card.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' },
+        { id: 5, name: 'Vale-presente', price: 100, icon: 'gift-card.svg', description: 'Vale-presente: você pode comprar para presentear um amigo com um item surpresa 😁' }
       ]
+    }
+  },
+  methods: {
+    buyProduct (id) {
+      this.productId = id
+      this.buyConfirm = true
     }
   }
 }
