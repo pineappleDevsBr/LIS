e23b1f61c4af4c925e4f758cc7bbb1beeb9ddd78
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2020-02-21 13:55:02 -0300

    Create register theme layout

--- api/app/Controllers/Http/Admin/ThemeController.js
+++ api/app/Controllers/Http/Admin/ThemeController.js
@@ -1,10 +1,53 @@
 'use strict'
 const Theme = use('App/Models/Theme')
 
+const labels = [
+  'name', 'description'
+]
+
 class ThemeController {
   async index({ view }) {
     const themes = await Theme.all();
     return view.render('themes', { themes: themes.toJSON() })
+  }
+
+  async store({ request, response, session }) {
+    const body = request.only(labels)
+
+    try {
+      await Theme.create(body);
+      session.flash({ created: true })
+      response.route('admin.themes');
+    } catch (err) {
+      response.send(err)
+    }
+  }
+
+  async update({ request, response }) {
+    const body = request.only(labels);
+    const { id } = request.all();
+
+    try {
+      const data = await Theme.find(id);
+      data.merge(body);
+      await data.save();
+      response.send(data)
+    } catch (err) {
+      response.send(err)
+    }
+  }
+
+  async delete({ request, response }) {
+    const { id } = request.all();
+
+    try {
+      const data = await Theme.find(id);
+      await data.delete();
+
+      response.status(204).end();
+    } catch (err) {
+      response.send(err)
+    }
   }
 }
 
--- api/app/Controllers/Http/Api/ThemeController.js
+++ api/app/Controllers/Http/Api/ThemeController.js
@@ -2,10 +2,6 @@
 const Theme = use('App/Models/Theme');
 const ThemeList = use('App/Models/ThemeList');
 
-const labels = [
-  'name', 'description'
-]
-
 class ThemeController {
   async index({ response }) {
     try {
@@ -24,48 +20,10 @@ class ThemeController {
         .where('user_id', auth.user.id)
         .innerJoin('themes', 'theme_lists.theme_id', 'themes.id')
         .fetch()
-      
+
       response.send(data);
     } catch(err) {
       response.send(err);
-    }
-  }
-
-  async store({ request, response }) {
-    const body = request.only(labels)
-
-    try {
-      const theme = await Theme.create(body);
-      response.status(201).send(theme)
-    } catch (err) {
-      response.send(err)
-    }
-  }
-
-  async update({ request, response }) {
-    const body = request.only(labels);
-    const { id } = request.all();
-
-    try {
-      const data = await Theme.find(id);
-      data.merge(body);
-      await data.save();
-      response.send(data)
-    } catch (err) {
-      response.send(err)
-    }
-  }
-
-  async delete({ request, response }) {
-    const { id } = request.all();
-
-    try {
-      const data = await Theme.find(id);
-      await data.delete();
-
-      response.status(204).end();
-    } catch (err) {
-      response.send(err)
     }
   }
 }
