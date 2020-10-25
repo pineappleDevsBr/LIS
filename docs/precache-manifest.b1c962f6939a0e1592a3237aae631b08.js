e24b553e12d42d50fab8f07393d474c42d096d15
Leandro C. Silva <leandrocesar49@hotmail.com>
2020-10-25 17:02:57 -0300

    Setup docs for deploy pwa on github pages

---
+++ docs/precache-manifest.b1c962f6939a0e1592a3237aae631b08.js
@@ -0,0 +1,470 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f2d8fbacd389e0d397",
+    "url": "css/3.6b87fc53.css"
+  },
+  {
+    "revision": "7e768548173ef6062b9e",
+    "url": "css/app.4f1d2ea9.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.a45108d3.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.cea99d3e.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.865f928c.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.2267169e.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bac8362e.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.49ae34d4.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.f2a09334.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.12a47ed5.woff2"
+  },
+  {
+    "revision": "886145f42303e2cb29322dc016783f07",
+    "url": "img/bg-404.606d481b.png"
+  },
+  {
+    "revision": "f057094a464f3539ea827de88cf825ea",
+    "url": "img/forgot-ilustra.76ea08c8.png"
+  },
+  {
+    "revision": "906edc992dd122534a65c70d69d2ac30",
+    "url": "img/icomoon.4e1e3f87.svg"
+  },
+  {
+    "revision": "2d02ea81139f8f58190783e1f1b52dad",
+    "url": "img/ilustra.d3ff3015.png"
+  },
+  {
+    "revision": "68c205992cafb9944af438166fb6ead5",
+    "url": "img/ilustra_step-1.f17aac97.png"
+  },
+  {
+    "revision": "29dcc7995b443df82a98667cc86eb871",
+    "url": "img/ilustra_step-2.9ea641df.png"
+  },
+  {
+    "revision": "c4614aade764a21336b1f74d012ae682",
+    "url": "img/ilustra_step-3.f46f95b2.png"
+  },
+  {
+    "revision": "2970de0ae54b70e8f1c584672bb4eb56",
+    "url": "img/login-ilustra.e5f232b0.png"
+  },
+  {
+    "revision": "afb17d84780e8370ac071d55f8ad7b09",
+    "url": "img/reset-ilustra.a045e087.png"
+  },
+  {
+    "revision": "fe4c12ba5303f689c80f673c7ada61ff",
+    "url": "img/without-internet.86a98d60.png"
+  },
+  {
+    "revision": "48f14bebfa898ea6e7427774c8dae23a",
+    "url": "index.html"
+  },
+  {
+    "revision": "119d8f0bbdeeba010db2",
+    "url": "js/1.119d8f0b.js"
+  },
+  {
+    "revision": "6e528e99c6c50d833add",
+    "url": "js/10.6e528e99.js"
+  },
+  {
+    "revision": "346015bbbf10e0a77c5e",
+    "url": "js/11.346015bb.js"
+  },
+  {
+    "revision": "d616908ce21e91b51707",
+    "url": "js/12.d616908c.js"
+  },
+  {
+    "revision": "c37f81c2fed280f1eba6",
+    "url": "js/13.c37f81c2.js"
+  },
+  {
+    "revision": "be83a85ddc8e1f7575fc",
+    "url": "js/14.be83a85d.js"
+  },
+  {
+    "revision": "37b375045398edd3f184",
+    "url": "js/15.37b37504.js"
+  },
+  {
+    "revision": "b86ac6c217e5057711d5",
+    "url": "js/16.b86ac6c2.js"
+  },
+  {
+    "revision": "b006dfd449bbc813f0c3",
+    "url": "js/17.b006dfd4.js"
+  },
+  {
+    "revision": "27f2d8fbacd389e0d397",
+    "url": "js/3.27f2d8fb.js"
+  },
+  {
+    "revision": "07b8d1d44c920262d589",
+    "url": "js/4.07b8d1d4.js"
+  },
+  {
+    "revision": "f86a0e5fda486c8d6c1b",
+    "url": "js/5.f86a0e5f.js"
+  },
+  {
+    "revision": "2a7afdacb22940dbc344",
+    "url": "js/6.2a7afdac.js"
+  },
+  {
+    "revision": "a6c1f96f395c72bee510",
+    "url": "js/7.a6c1f96f.js"
+  },
+  {
+    "revision": "b39a01a054dd1e410f0f",
+    "url": "js/8.b39a01a0.js"
+  },
+  {
+    "revision": "d4b992293deeaf942b42",
+    "url": "js/9.d4b99229.js"
+  },
+  {
+    "revision": "7e768548173ef6062b9e",
+    "url": "js/app.f33b67eb.js"
+  },
+  {
+    "revision": "8657422636e8841c2aae",
+    "url": "js/vendor.86574226.js"
+  },
+  {
+    "revision": "d7b37e8ee6083886e8c8c7e446a4af9d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "2cb546fca9f56235ff22f15f46c1c189",
+    "url": "statics/activities/content.svg"
+  },
+  {
+    "revision": "ff0640892cd74d7aa97c2bd5696d4cd9",
+    "url": "statics/activities/headphones.svg"
+  },
+  {
+    "revision": "1c5cecbe1e96830ed80b9ab22ab83fc5",
+    "url": "statics/activities/interactive-content.svg"
+  },
+  {
+    "revision": "9b441c62d7850b82cb919e140f645498",
+    "url": "statics/activities/quiz.svg"
+  },
+  {
+    "revision": "0d119719d4d53f2e62fc6535cb544b74",
+    "url": "statics/activities/read.svg"
+  },
+  {
+    "revision": "a4e98bb4c2f3e0adc5549d3aed961818",
+    "url": "statics/activities/tutorials.png"
+  },
+  {
+    "revision": "e78a462fd9c5531881ddba75c19fb081",
+    "url": "statics/blocked/blocked.png"
+  },
+  {
+    "revision": "870f4500e701d7d2fe01c8198c590ab4",
+    "url": "statics/credits/icons/chest.svg"
+  },
+  {
+    "revision": "8a3ede898171a07fedc11c587b82ed56",
+    "url": "statics/credits/icons/coin.svg"
+  },
+  {
+    "revision": "2cb546fca9f56235ff22f15f46c1c189",
+    "url": "statics/credits/icons/complete.svg"
+  },
+  {
+    "revision": "4269b02ec24c29e449eaa561b3f97fee",
+    "url": "statics/credits/icons/gift.svg"
+  },
+  {
+    "revision": "448e3acfb166fd1c8f46811cb3a4af1f",
+    "url": "statics/credits/icons/hub.svg"
+  },
+  {
+    "revision": "ff0640892cd74d7aa97c2bd5696d4cd9",
+    "url": "statics/credits/icons/listening.svg"
+  },
+  {
+    "revision": "35ff2977a03d8854740a67328b2b88d7",
+    "url": "statics/credits/icons/money.svg"
+  },
+  {
+    "revision": "9b441c62d7850b82cb919e140f645498",
+    "url": "statics/credits/icons/quiz.svg"
+  },
+  {
+    "revision": "0d119719d4d53f2e62fc6535cb544b74",
+    "url": "statics/credits/icons/reading.svg"
+  },
+  {
+    "revision": "0cb6b3c9a8b84303efa3e100f540b0c3",
+    "url": "statics/credits/icons/settings.svg"
+  },
+  {
+    "revision": "fc55566c1f7ea2a70e35d0d7b900cb43",
+    "url": "statics/credits/icons/store.svg"
+  },
+  {
+    "revision": "5206ec9137a619e1a2ed2cf27d8f3cdc",
+    "url": "statics/credits/icons/xp.svg"
+  },
+  {
+    "revision": "68c205992cafb9944af438166fb6ead5",
+    "url": "statics/credits/illustrations/acess-step-1.png"
+  },
+  {
+    "revision": "29dcc7995b443df82a98667cc86eb871",
+    "url": "statics/credits/illustrations/acess-step-2.png"
+  },
+  {
+    "revision": "c4614aade764a21336b1f74d012ae682",
+    "url": "statics/credits/illustrations/acess-step-3.png"
+  },
+  {
+    "revision": "327709933d3cfc361d8d45056d3a9ed8",
+    "url": "statics/credits/illustrations/e-mail_welcome.png"
+  },
+  {
+    "revision": "886145f42303e2cb29322dc016783f07",
+    "url": "statics/credits/illustrations/error-404.png"
+  },
+  {
+    "revision": "f057094a464f3539ea827de88cf825ea",
+    "url": "statics/credits/illustrations/forgot.png"
+  },
+  {
+    "revision": "38de24ff6e12ef1659d386c8b3e2ef2a",
+    "url": "statics/credits/illustrations/hub-modal-convite.png"
+  },
+  {
+    "revision": "2970de0ae54b70e8f1c584672bb4eb56",
+    "url": "statics/credits/illustrations/login.png"
+  },
+  {
+    "revision": "afb17d84780e8370ac071d55f8ad7b09",
+    "url": "statics/credits/illustrations/reset.png"
+  },
+  {
+    "revision": "2d02ea81139f8f58190783e1f1b52dad",
+    "url": "statics/credits/illustrations/welcome.png"
+  },
+  {
+    "revision": "fe4c12ba5303f689c80f673c7ada61ff",
+    "url": "statics/credits/illustrations/without-internet.png"
+  },
+  {
+    "revision": "448e3acfb166fd1c8f46811cb3a4af1f",
+    "url": "statics/hub/bracelet.svg"
+  },
+  {
+    "revision": "b73fe685aec6544edb62c418deec0e26",
+    "url": "statics/hub/invite-1.png"
+  },
+  {
+    "revision": "38de24ff6e12ef1659d386c8b3e2ef2a",
+    "url": "statics/hub/invite.png"
+  },
+  {
+    "revision": "cde3a61f6ede02734d76d03ce0a9b65a",
+    "url": "statics/hub/invite_old.png"
+  },
+  {
+    "revision": "8729380dba78733bfe16530ab92b92a7",
+    "url": "statics/icons/android-icon-144x144.png"
+  },
+  {
+    "revision": "48d14b223669815c8eae53f883d07dbd",
+    "url": "statics/icons/android-icon-192x192.png"
+  },
+  {
+    "revision": "65197c75d689a316215985e77499373e",
+    "url": "statics/icons/android-icon-36x36.png"
+  },
+  {
+    "revision": "327b626c326117019a470e3f91ee3a55",
+    "url": "statics/icons/android-icon-48x48.png"
+  },
+  {
+    "revision": "1e70e703914b743878132c20314fdb0b",
+    "url": "statics/icons/android-icon-72x72.png"
+  },
+  {
+    "revision": "95a295b2e39cb963f697cb56be9e32a8",
+    "url": "statics/icons/android-icon-96x96.png"
+  },
+  {
+    "revision": "bab18d3346935621ba1f81ddd75eade7",
+    "url": "statics/icons/apple-icon-114x114.png"
+  },
+  {
+    "revision": "87e2d739c59a6b31a02fd2994c37aea0",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "8729380dba78733bfe16530ab92b92a7",
+    "url": "statics/icons/apple-icon-144x144.png"
+  },
+  {
+    "revision": "ae3e7ea37f0de561437bd162449c7a24",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "4300c6c136c06abcd781aa18644dd7a3",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "33b42fd890c40b2cd6b7c3ef8feb7dc0",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "58c4c7c3dfd4ba417e7ca9f46c76a5cb",
+    "url": "statics/icons/apple-icon-57x57.png"
+  },
+  {
+    "revision": "2025376e87c9ca6531dd6b68eea19034",
+    "url": "statics/icons/apple-icon-60x60.png"
+  },
+  {
+    "revision": "1e70e703914b743878132c20314fdb0b",
+    "url": "statics/icons/apple-icon-72x72.png"
+  },
+  {
+    "revision": "661979083e8cf0efe3ec2a2f36c4c954",
+    "url": "statics/icons/apple-icon-76x76.png"
+  },
+  {
+    "revision": "faa6d0b7599b528ac28fac7267046525",
+    "url": "statics/icons/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "faa6d0b7599b528ac28fac7267046525",
+    "url": "statics/icons/apple-icon.png"
+  },
+  {
+    "revision": "320130eb5a899d49b933491068b97a07",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "e23fc81d91dd9a61ceb89551f2919efb",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "95a295b2e39cb963f697cb56be9e32a8",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "bfbf1e4e23d26fa6149a7de62c56e478",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "1be07e62226c8f6a7086f132d9be1642",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "a1b423855a450ae7adbd9d2a782e0dfb",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "9f33ebc999e51d0dc2975fc573d53a0b",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "a126be0d50ed682c3129e0b8b31f680a",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "02abd24e7c8638e848d96a6bf78d2c27",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "8729380dba78733bfe16530ab92b92a7",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "4bf144ecf90dedec4713b320e70af149",
+    "url": "statics/icons/ms-icon-150x150.png"
+  },
+  {
+    "revision": "71fcdfd6e03e9ba46e839f10c6c43dbb",
+    "url": "statics/icons/ms-icon-310x310.png"
+  },
+  {
+    "revision": "bcab923050fd2f0b92a9bd2794ab17a7",
+    "url": "statics/icons/ms-icon-70x70.png"
+  },
+  {
+    "revision": "de8107bed7f087fb443d8e36855e7467",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "a6aaab065312789b77a97f6b187f3bdb",
+    "url": "statics/nationalities/1.svg"
+  },
+  {
+    "revision": "5538bd3a07e4d53b0efb119d74218bdc",
+    "url": "statics/profile/achievements/book.svg"
+  },
+  {
+    "revision": "415fa9e8c540dd5fcbb814eceaedeadf",
+    "url": "statics/profile/achievements/shield.svg"
+  },
+  {
+    "revision": "f442ca8b0944bffb7338690b8455f424",
+    "url": "statics/profile/cog.svg"
+  },
+  {
+    "revision": "0cb6b3c9a8b84303efa3e100f540b0c3",
+    "url": "statics/settings/gear.svg"
+  },
+  {
+    "revision": "8a3ede898171a07fedc11c587b82ed56",
+    "url": "statics/store/coin.svg"
+  },
+  {
+    "revision": "fc55566c1f7ea2a70e35d0d7b900cb43",
+    "url": "statics/store/online-store.svg"
+  },
+  {
+    "revision": "870f4500e701d7d2fe01c8198c590ab4",
+    "url": "statics/store/products/chest.svg"
+  },
+  {
+    "revision": "35ff2977a03d8854740a67328b2b88d7",
+    "url": "statics/store/products/money.svg"
+  },
+  {
+    "revision": "4269b02ec24c29e449eaa561b3f97fee",
+    "url": "statics/store/products/present.svg"
+  },
+  {
+    "revision": "5206ec9137a619e1a2ed2cf27d8f3cdc",
+    "url": "statics/store/products/xp.svg"
+  }
+]);
